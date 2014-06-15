db44599659884f5f3a2c4d0b1a20cf1ee56edcf1
George Garside <grg.garside@gmail.com>
2014-06-15 20:31:21 +0100

    Add iOS status bar plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "window.plugins.iAd"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.rjfun.cordova.plugin.iad": "0.1.2"
+    "com.rjfun.cordova.plugin.iad": "0.1.2",
+    "org.apache.cordova.statusbar": "0.1.7-dev"
 }
 // BOTTOM OF METADATA
 });
