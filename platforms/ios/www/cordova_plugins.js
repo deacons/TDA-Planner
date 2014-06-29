15043cc79faa99a111dfd4a076e8f54c0976dcb8
George Garside <grg.garside@gmail.com>
2014-06-29 09:09:46 +0100

    Remove iAd

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.rjfun.cordova.plugin.iad/www/iAd.js",
-        "id": "com.rjfun.cordova.plugin.iad.iAd",
-        "clobbers": [
-            "window.plugins.iAd"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
         "id": "org.apache.cordova.statusbar.statusbar",
         "clobbers": [
@@ -18,7 +11,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.rjfun.cordova.plugin.iad": "0.1.2",
     "org.apache.cordova.statusbar": "0.1.7-dev"
 }
 // BOTTOM OF METADATA
