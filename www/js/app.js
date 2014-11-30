b04275ecab81e3d0a064cb775aea3fe86b5a1fec
George Garside <grg.garside@gmail.com>
2014-11-30 15:36:36 +0000

    Refactor myApp to appPlanner

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,17 @@
 // Initialize your app
-var myApp = new Framework7();
+var appPlanner = new Framework7();
 
 // Export selectors engine
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = appPlanner.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
+appPlanner.onPageInit('login', function (page) {
 });
 
 // Generate dynamic page
