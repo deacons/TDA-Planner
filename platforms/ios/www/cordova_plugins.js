6b74d780728ae3226f24ca3dc9bddf864da7e7b7
George Garside <grg.garside@gmail.com>
2014-11-30 16:26:27 +0000

    Add org.apache.cordova.network-information plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,28 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.statusbar": "0.1.7-dev"
+    "org.apache.cordova.statusbar": "0.1.7-dev",
+    "org.apache.cordova.network-information": "0.2.13"
 }
 // BOTTOM OF METADATA
 });
