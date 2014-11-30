e3d79cb59859f80ebd78e64591b3ec2fd372448f
George Garside <grg.garside@gmail.com>
2014-11-30 16:13:10 +0000

    Spaces to tabs
    Oh dear

--- www/js/app.js
+++ www/js/app.js
@@ -6,53 +6,53 @@ var $$ = Dom7;
 
 // Add view
 var mainView = appPlanner.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+	// Because we use fixed-through navbar we can enable dynamic navbar
+	dynamicNavbar: true
 });
 
 function store(){
-    var inputUser = document.getElementById("username");
-    localStorage.setItem("username", inputUser.value);
-    var inputPass = document.getElementById("password");
-    localStorage.setItem("password", inputPass.value);
-    return true;
+	var inputUser = document.getElementById("username");
+	localStorage.setItem("username", inputUser.value);
+	var inputPass = document.getElementById("password");
+	localStorage.setItem("password", inputPass.value);
+	return true;
 }
 
 var inputUser = localStorage.getItem("username");
 var inputPass = localStorage.getItem("password");
 
 appPlanner.onPageInit('login', function (page) {
-    if (inputUser) {
-        document.getElementById('username').value = inputUser;
-        document.getElementById('password').value = inputPass;
-    }
+	if (inputUser) {
+		document.getElementById('username').value = inputUser;
+		document.getElementById('password').value = inputPass;
+	}
 });
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
 	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
+		'<!-- Top Navbar-->' +
+		'<div class="navbar">' +
+		'  <div class="navbar-inner">' +
+		'    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+		'    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+		'  </div>' +
+		'</div>' +
+		'<div class="pages">' +
+		'  <!-- Page, data-page contains page name-->' +
+		'  <div data-page="dynamic-pages" class="page">' +
+		'    <!-- Scrollable page content-->' +
+		'    <div class="page-content">' +
+		'      <div class="content-block">' +
+		'        <div class="content-block-inner">' +
+		'          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+		'          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+		'        </div>' +
+		'      </div>' +
+		'    </div>' +
+		'  </div>' +
+		'</div>'
+	);
 	return;
 }
